d3be7885b7c29456ad0e120e998672aa5ce5ad56
Giorgi Okropiridze <giorgiokropiridze@credo.ge>
2026-05-27 17:15:11 +0400

    [Giorgi O.] Add Homework3.java with author info

---
+++ src/Homework3.java
@@ -0,0 +1,7 @@
+public class Homework3 {
+    public static void main(String[] args) {
+        String author = "Giorgi Okropiridze";
+        int currentYear = 2026;
+        System.out.println("[" + author + "] First Pull Request — " + currentYear);
+    }
+}
